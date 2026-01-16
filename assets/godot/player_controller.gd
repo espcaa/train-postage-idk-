20e6f85c824be981acd076c764dceb82e0e83c5f
alice <122629939+espcaa@users.noreply.github.com>
2026-01-17 00:44:06 +0100

    first perso ncotnroller :3

--- assets/godot/player_controller.gd
+++ assets/godot/player_controller.gd
@@ -1,27 +1,50 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
+const ACCEL = 10.0
 const JUMP_VELOCITY = 4.5
+
+@export var MOUSE_SENSITIVITY: float = 0.1
+
+var mouse_input := Vector2.ZERO
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		mouse_input += event.relative
+
+
+func _process(_delta):
+	rotate_y(deg_to_rad(-mouse_input.x * MOUSE_SENSITIVITY))
+
+	var camera = $Pivot/Camera3D
+	camera.rotation.x -= deg_to_rad(mouse_input.y * MOUSE_SENSITIVITY)
+	camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+
+	mouse_input = Vector2.ZERO
 
 
 func _physics_process(delta: float) -> void:
+	# gravity stuff \o/
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	# physical movement
+	var input_dir := Input.get_vector("left", "right", "forwards", "backwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if not is_on_floor():
+		direction *= 0.3
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
