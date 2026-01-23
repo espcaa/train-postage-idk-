7ceb157d548990a23011c29c279d28dad2505ada
alice <122629939+espcaa@users.noreply.github.com>
2026-01-23 10:40:40 +0100

    some outlining working

--- assets/godot/player_controller.gd
+++ assets/godot/player_controller.gd
@@ -9,6 +9,7 @@ const AIR_ACCEL = 3.0
 @export var MOUSE_SENSITIVITY: float = 0.1
 
 var mouse_input := Vector2.ZERO
+var current_highlighted: PickableObject = null
 
 
 func _ready() -> void:
@@ -18,14 +19,14 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		mouse_input = event.relative
-		print(mouse_input)
+
 
 func _process(_delta):
 	var joy_input := Input.get_vector("look-left", "look-right", "look-up", "look-down")
-	if joy_input != Vector2(0,0):
+	if joy_input != Vector2(0, 0):
 		mouse_input = joy_input * 20.0
 	_update_camera()
-	
+
 
 func _update_camera():
 	rotate_y(deg_to_rad(-mouse_input.x * MOUSE_SENSITIVITY))
@@ -59,5 +60,28 @@ func _physics_process(delta: float) -> void:
 			velocity.x = move_toward(velocity.x, 0, SPEED * delta * ACCEL)
 			velocity.z = move_toward(velocity.z, 0, SPEED * delta * ACCEL)
 		# If in air and no input, we don't add any friction (preserving momentum)
+
+	# picking stuff
+
+	if $Pivot/Camera3D/RayCast3D.is_colliding():
+		var collider = $Pivot/Camera3D/RayCast3D.get_collider()
+		for i in collider.get_children():
+			if i is PickableObject and i != current_highlighted:
+				print(str(i) + " just lighted")
+				i.highlight()
+				current_highlighted = i
+				break
+
+		for obj in get_tree().get_nodes_in_group("pickable_objects"):
+			if obj != collider and obj is PickableObject and obj != current_highlighted:
+				print(str(obj) + " just unlighted")
+				obj.unhighlight()
+	else:
+		for obj in get_tree().get_nodes_in_group("pickable_objects"):
+			if obj is PickableObject and current_highlighted != null:
+				print(str(obj) + " just unlighted")
+				obj.unhighlight()
+				if obj == current_highlighted:
+					current_highlighted = null
 
 	move_and_slide()
--- scenes/pickable_object.gd
+++ scenes/pickable_object.gd
@@ -3,36 +3,40 @@ extends Node3D
 
 @export var highlighted_meshes: Array[MeshInstance3D] = []
 var outline_material: StandardMaterial3D
+var overlay_material: StandardMaterial3D
 
 
 func _ready() -> void:
 	outline_material = StandardMaterial3D.new()
 	outline_material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	outline_material.albedo_color = Color(1, 1, 0)  # Yellow
+	outline_material.albedo_color = Color(1, 1, 1)  # Yellow
 	outline_material.cull_mode = BaseMaterial3D.CULL_FRONT
 	outline_material.grow = true
 	outline_material.grow_amount = 0.03
-
-	outline_material.stencil_mode = BaseMaterial3D.STENCIL_MODE_CUSTOM
-	outline_material.stencil_compare = BaseMaterial3D.STENCIL_COMPARE_NOT_EQUAL
-	outline_material.stencil_reference = 1
-
+	
+	overlay_material = StandardMaterial3D.new()
+	overlay_material.stencil_mode = BaseMaterial3D.STENCIL_MODE_OUTLINE
+	overlay_material.stencil_reference = 1
+	overlay_material.next_pass = outline_material
+	
 
 func set_highlighted(highlighted: bool) -> void:
+	print("setting highlightubbgjhsdb"+ str(highlighted))
 	for mesh in highlighted_meshes:
 		if not mesh:
 			continue
 
 		var base_mat = mesh.get_active_material(0)
-
+		
 		if highlighted:
 			if base_mat:
 				base_mat.stencil_mode = BaseMaterial3D.STENCIL_MODE_CUSTOM
 				base_mat.stencil_flags = BaseMaterial3D.STENCIL_FLAG_WRITE
 				base_mat.stencil_reference = 1
 
-			mesh.material_overlay = outline_material
+			mesh.material_overlay = overlay_material
 		else:
+			
 			# Clean up
 			if base_mat:
 				base_mat.stencil_mode = BaseMaterial3D.STENCIL_MODE_DISABLED
